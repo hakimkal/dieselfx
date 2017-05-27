0d35ca03eaf73501a153d9998f45d7040083fa31
Abdulhakim Haliru <hakimkal@gmail.com>
2017-05-27 21:21:06 +0100

    completed basic CRUD service feature

--- src/main/java/com/dieselfx/services/DailyPriceService.java
+++ src/main/java/com/dieselfx/services/DailyPriceService.java
@@ -1,6 +1,11 @@
 package com.dieselfx.services;
 
+import com.dieselfx.domains.DailyPrice;
+import com.dieselfx.repositories.DailyPriceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by macbook on 5/27/17.
@@ -8,4 +13,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DailyPriceService {
+
+    @Autowired
+    private DailyPriceRepository dailyPriceRepository;
+
+    public DailyPrice save(DailyPrice dailyPrice) {
+        return dailyPriceRepository.save(dailyPrice);
+    }
+
+    public Iterable<DailyPrice> findAll(){
+        return dailyPriceRepository.findAll();
+    }
+
+    public DailyPrice findOne(Long id){
+        return dailyPriceRepository.findOne(id);
+    }
+
+    public void delete(Long id){
+        dailyPriceRepository.delete(id);
+    }
 }
--- src/main/java/com/dieselfx/services/InfoSourceService.java
+++ src/main/java/com/dieselfx/services/InfoSourceService.java
@@ -1,5 +1,9 @@
 package com.dieselfx.services;
 
+import com.dieselfx.domains.DailyPrice;
+import com.dieselfx.domains.InfoSource;
+import com.dieselfx.repositories.InfoSourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,6 +11,23 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class InfoSourceService  {
+@Autowired
+    InfoSourceRepository infoSourceRepository;
 
+public InfoSource save(InfoSource infoSource){
+    return  infoSourceRepository.save(infoSource);
+}
+
+    public Iterable<InfoSource> findAll(){
+        return infoSourceRepository.findAll();
+    }
+
+    public InfoSource findOne(Long id){
+        return infoSourceRepository.findOne(id);
+    }
+
+    public void delete(Long id){
+       infoSourceRepository.delete(id);
+    }
 
 }

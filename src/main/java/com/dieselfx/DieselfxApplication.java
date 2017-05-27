a640c47058d538ec62d82347c431a91142dfb1d5
Abdulhakim Haliru <hakimkal@gmail.com>
2017-05-27 13:35:03 +0100

    first push

---
+++ src/main/java/com/dieselfx/DieselfxApplication.java
@@ -0,0 +1,12 @@
+package com.dieselfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieselfxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DieselfxApplication.class, args);
+	}
+}
